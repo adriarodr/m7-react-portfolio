8020e8cc26fc812d5fbcf551c896fcbb7cd5d8ff
adriarodr <158720918+adriarodr@users.noreply.github.com>
2026-07-01 19:22:22 -0400

    feat: add about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import About from './components/About';
+import Experience from './components/Experience';
 import Header from './components/Header';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
+
       <main>
         <p>Welcome to my portfolio</p>
+        <About />
+        <Projects />
+        <Skills />
+        <Experience />
       </main>
+
+      <Footer />
     </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <section id="about-me">
+      <h2>About Me</h2>
+      <p>Add a paragraph about myself and my current skill set.</p>
+      {/* <img src="./" alt="." /> */}
+    </section>
+  );
+}
+
+export default About;
