5bb59f73c1e8897541dbb6c13a7daf04d3459672
adriarodr <158720918+adriarodr@users.noreply.github.com>
2026-07-02 17:47:14 -0400

    fix: move isExternal to link componenent

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,25 @@ import Nav from './Nav';
 
 function Footer() {
   const footer_links = [
-    { id: 0, text: 'GitHub', url: 'https://github.com/adriarodr' },
-    { id: 1, text: 'Linkedin', url: 'https://www.linkedin.com/in/adriarodr/' },
+    {
+      id: 0,
+      text: 'GitHub',
+      url: 'https://github.com/adriarodr',
+      isExternal: true,
+    },
+    {
+      id: 1,
+      text: 'Linkedin',
+      url: 'https://www.linkedin.com/in/adriarodr/',
+      isExternal: true,
+    },
   ];
 
   const currentYear = new Date().getFullYear();
 
   return (
     <footer>
-      <Nav id="footer-nav" links={footer_links} isExternal={true} />
+      <Nav id="footer-nav" links={footer_links} />
       <p>&copy; {currentYear} Adrian Rodriguez. All Rights Reserved.</p>
     </footer>
   );
